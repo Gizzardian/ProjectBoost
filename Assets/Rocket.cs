ab87d742a103c43676476544e6b21827a52764bd
Jae Taylor <gizzardian@gmail.com>
2019-06-29 15:37:49 -0700

    added thrust sound

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -4,13 +4,21 @@ using UnityEngine;
 
 public class Rocket : MonoBehaviour
 {
+    // set physics
     Rigidbody rigidBody;
+
+    // set sound
+    AudioSource rocketThrust;
+    bool playThurst;
 
     // Start is called before the first frame update
     void Start()
     {
         // give us access to rigid body of Rocket Ship
         rigidBody = GetComponent<Rigidbody>();
+
+        // get access to sound
+        rocketThrust = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -26,6 +34,14 @@ public class Rocket : MonoBehaviour
         if (Input.GetKey(KeyCode.Space))
         {
             rigidBody.AddRelativeForce(Vector3.up);
+            if (!rocketThrust.isPlaying)  // bang signifys is NOT, stops from trying to play repeatedly
+            {
+                rocketThrust.Play();
+            }
+        }
+        else 
+        {
+            rocketThrust.Stop();
         }
         
         if (Input.GetKey(KeyCode.A))
